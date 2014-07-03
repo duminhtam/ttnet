b551dae5f3b6aca593ed91c92ba516c96e5d4712
duminhtam <tamdu@chotot.vn>
2014-07-03 17:09:58 +0700

    code

---
+++ app/controllers/app.go
@@ -0,0 +1,41 @@
+package controllers
+
+import "github.com/revel/revel"
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Facebook() revel.Result {
+	greeting := "Hello Tam"
+	return c.Render(greeting)
+}
+func (c App) Index() revel.Result {
+	greeting := "Hello Tam"
+	return c.Render(greeting)
+}
+
+func (c App) Hello(myName string) revel.Result {
+	c.Validation.Required(myName).Message("myName is required")
+	c.Validation.MinSize(myName, 4).Message("4 len!!")
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+
+	var data = map[string]map[string]string{
+		"account":{
+			"name":myName,
+			"age":"27",
+		},
+		"ads":{
+			"title":"Ads Title",
+			"body":"Sell s.th",
+		},
+	}
+
+	return c.Render(data)
+}
+
